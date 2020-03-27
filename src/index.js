a9aa3c5bd09dfed39d8c93d197e2757dc87a5054
Denis <soldugeev.d@cronix.ms>
2020-03-27 17:57:37 +0300

    add_scripts_slice_text

--- src/index.js
+++ src/index.js
@@ -1,4 +1,36 @@
 import  $ from 'jquery'
 import './styles/main.scss'
+import './assets/js/script'
 import './assets/img/1.png'
+
+
+const btn = document.querySelector(".reviews__more");
+const more = document.querySelector(".more");
+const arrow = document.querySelector(".arrow-down");
+const text = document.querySelector(".product__reviews");
+const textLength = text.innerHTML;
+
+function sliceText() {
+    if (textLength.length >= 150) {
+        let  sliceText = textLength.substr(0, 150);
+        text.innerHTML = sliceText;
+
+    }
+}
+sliceText();
+
+btn.onclick = function () {
+      if (text.innerHTML.length <= 150) {
+          text.innerHTML = textLength;
+          more.innerHTML = 'Скрыть';
+          arrow.style.transform = 'rotate(45deg)'
+
+      } else {
+          let  sliceText = textLength.substr(0, 150);
+          text.innerHTML = sliceText;
+          more.innerHTML = 'Подробнее';
+          arrow.style.transform = 'rotate(225deg)'
+      }
+
+};
 
